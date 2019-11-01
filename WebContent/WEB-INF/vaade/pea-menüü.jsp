b6014b805fb4fbe802b4cf1f9a6714960435989b
Rene Roost <roost.rene@gmail.com>
2019-11-01 09:42:27 +0200

    Kontrolleri ja vaate loomine

---
+++ WebContent/WEB-INF/vaade/pea-menüü.jsp
@@ -0,0 +1,9 @@
+ <!DOCTYPE html>
+ <html>
+ 
+ <body>
+ 
+ <h2>Spring MVC demo - Koduleht</h2>
+ </body>
+ 
+ </html>
---
+++ src/ee/harjutamine/spring/mvc/KoduKontroller.java
@@ -0,0 +1,14 @@
+package ee.harjutamine.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class KoduKontroller {
+	
+	@RequestMapping("/")
+	public String naitaLehte() {
+		return "pea-menüü";
+	}
+
+}
